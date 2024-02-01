9ef79c6eda1b6f6c62b44234d6137f5ce36f4399
Huy Huynh <htqhuy1762@gmail.com>
2024-02-01 10:51:57 +0700

    Delete user API

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const routerAPI = express.Router();
-const { getUsersAPI, postCreateUserAPI, putUpdateUserAPI } = require('../controllers/apiController');
+const { getUsersAPI, postCreateUserAPI, putUpdateUserAPI, deleteUserAPI } = require('../controllers/apiController');
 
 
 routerAPI.get('/users', getUsersAPI);
 routerAPI.post('/users', postCreateUserAPI);
 routerAPI.put('/users', putUpdateUserAPI);
+routerAPI.delete('/users', deleteUserAPI);
 
 
 module.exports = routerAPI; // export default
